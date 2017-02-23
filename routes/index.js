1c396cb3049a001eba26cc8b72a0f93ed0668dd5
Chiao Fu <joannelovecat@gmail.com>
2017-02-22 22:09:58 -0800

    add /min

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,13 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+	projects["min"] = false;
+	console.log("normal! " + projects.min);
+  	res.render('index', projects);
+};
+
+exports.viewMin = function(req, res){
+	projects["min"] = true;
+	console.log("min! " + projects.min);
   	res.render('index', projects);
 };
